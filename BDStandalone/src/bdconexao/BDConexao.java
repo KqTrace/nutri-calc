df7151073453b7c5f66265d2154b2b8587f82522
KqTrace <31016004+KqTrace@users.noreply.github.com>
2017-08-14 21:58:10 -0300

    Update-Database

--- BDStandalone/src/bdconexao/BDConexao.java
+++ BDStandalone/src/bdconexao/BDConexao.java
@@ -2,23 +2,63 @@ package bdconexao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BDConexao {
-
-	public void conectar() {
+	
+	public static Connection criarConexao() {
 		try {
-			Class.forName("org.hsqldb.jdbcDriver");
-			Connection con = DriverManager.getConnection("jdbc:hsqldb:file:/bancoDados/MyServerDB", "sa", "");
-			System.out.println("Conexão realizada !");
+			 Class.forName("org.hsqldb.jdbcDriver");
+			 Connection con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/MyServerDB", "SA", "");
+			 System.out.println("Conexão realizada !");
+			 
+			    Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT * FROM Grupo");
+				
+				while (rs.next()) {
+					
+					int id = rs.getInt(1);
+					String nm_grupo = rs.getString(2);
+					
+					System.out.printf("ID do Grupo de Alimentos: %d | Nome do grupo: %s", id, nm_grupo);
+				}
+				
+			 return con;
 		}catch (ClassNotFoundException e) {
 			System.out.println("Erro ao carregar o Driver JDBC.");
+			return null;
 		}catch(SQLException f) {
 			System.out.println("Erro de SQL: " + f);
 			f.printStackTrace();
+			return null;
 		}
 	}
 	
+	public void consultarGrupo(String sql) {
+		try {
+			
+			Connection dbConnection = this.getDBConnection();
+			Statement stmt = this.getDBConnection().createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+				
+				int id = rs.getInt(1);
+				String nm_grupo = rs.getString(2);
+				
+				System.out.printf("ID do Grupo de Alimentos: %d | Nome do grupo: %s", id, nm_grupo);
+			}
+			
+		} catch (SQLException e) {
+		}
+		
+	}
 	
+	private Connection getDBConnection() {
+		return con;
+	}
 	
 }
